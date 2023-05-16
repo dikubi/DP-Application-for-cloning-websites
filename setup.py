c99b85ff1a3658e9bfd19026e5201d1862332058
Dita Kubíčková <80394709+dikubi@users.noreply.github.com>
2023-05-16 09:25:49 +0200

    Change of folder

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 with open('requirements.txt', 'r') as f:
     install_requires = f.read().splitlines()
 
-setuptools.setup(name='DP-program',
+setuptools.setup(name='DP-Application-for-cloning-websites',
                  packages=[''],
                  install_requires=install_requires)
